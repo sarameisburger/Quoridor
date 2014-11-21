d939f25115dc865a2eb93acbe012e7046fda08e8
kramerl17 <kramerl17@up.edu>
2014-11-20 20:54:11 -0800

    checkIfGameOver

--- src/edu/up/cs301/quoridor/QDLocalGame.java
+++ src/edu/up/cs301/quoridor/QDLocalGame.java
@@ -1,5 +1,6 @@
 package edu.up.cs301.quoridor;
 
+import android.graphics.Point;
 import edu.up.cs301.game.GamePlayer;
 import edu.up.cs301.game.LocalGame;
 import edu.up.cs301.game.actionMsg.GameAction;
@@ -24,6 +25,9 @@ public class QDLocalGame extends LocalGame implements QDGame {
 	// the number of moves that have been played so far, used to
 	// determine whether the game is over
 	protected int moveCount;
+	
+	private Point[] pawns;
+
 
 	/**
 	 * Constructor for the TTTLocalGame.
@@ -47,65 +51,37 @@ public class QDLocalGame extends LocalGame implements QDGame {
 	 */
 	@Override
 	protected String checkIfGameOver() {
-
-		// the idea is that we simultaneously look at a row, column and
-		// a diagonal, using the variables 'rowToken', 'colToken' and
-		// 'diagToken'; we do this three times so that we get all three
-		// rows, all three columns, and both diagonals.  (The way the
-		// math works out, one of the diagonal tests tests the middle
-		// column.)  The respective variables get set to ' ' unless
-		// all characters in the line that have currently been seen are
-		// identical; in this case the variable contains that character
-
-		// the character that will eventually contain an 'X' or 'O' if we
-		// find a winner
-		char resultChar = ' ';
-
-		// to all three lines in the current group
-		for (int i = 0; i < 3; i++) {
-			// get the initial character in each line
-//			char rowToken = state.getPiece(i,0);
-//			char colToken = state.getPiece(0,i);;
-//			char diagToken = state.getPiece(0,i);
-//			// determine the direction that the diagonal moves
-//			int diagDelta = 1-i;
-//			// look for matches for each of the three positions in each
-//			// of the current lines; set the corresponding variable to ' '
-//			// if a mismatch is found
-//			for (int j = 1; j < 3; j++) {
-//				if (state.getPiece(i,j) != rowToken) rowToken = ' ';
-//				if (state.getPiece(j,i) != colToken) colToken = ' ';
-//				if (state.getPiece(j, i+(diagDelta*j)) != diagToken) diagToken = ' ';
-//			}
-
-			////////////////////////////////////////////////////////////
-			// At this point, if any of our three variables is non-blank
-			// then we have found a winner.
-			////////////////////////////////////////////////////////////
-
-			// if we find a winner, indicate such by setting 'resultChar'
-			// to the winning mark.
-//			if (rowToken != ' ') resultChar = rowToken;
-//			else if (colToken != ' ') resultChar = colToken;
-//			else if (diagToken != ' ') resultChar = diagToken;
-		}
-
-		// if resultChar is blank, we found no winner, so return null,
-		// unless the board is filled up. In that case, it's a cat's game.
-		if (resultChar == ' ') {
-			if  (moveCount >= 1000) {
-				// no winner, but all 9 spots have been filled
-				return "It's a cat's game.";
+		String resultStr = ""; 
+		pawns = state.getPawns(); 
+		
+		//iterate through pawns on the board
+		for (int i = 0; i < pawns.length; i++){
+			//check to see if each pawn has reached it winning x, y values 
+			if (i == 0){ //if pawn 0
+				if (pawns[i].x == 4 && pawns[i].y == 0){
+					resultStr = "Player: " + (i + 1) + "is the winner"; 
+				}
 			}
-			else {
-				return null; // no winner, but game not over
+			if (i == 1){ //if pawn 1
+				if (pawns[i].x == 4 && pawns[i].y == 8){
+					resultStr = "Player: " + (i + 1) + "is the winner"; 
+				}
+			}
+			if (i == 2){
+				if (pawns[i].x == 8 && pawns[i].y == 4){
+					resultStr = "Player: " + (i + 1) + "is the winner"; 
+				}
+			}
+			if (i == 3){
+				if (pawns[i].x == 0 && pawns[i].y == 4){
+					resultStr = "Player: " + (i + 1) + "is the winner"; 
+				}
 			}
 		}
-
-		// if we get here, then we've found a winner, so return the 0/1
-		// value that corresponds to that mark; then return a message
-		int gameWinner = resultChar == mark[0] ? 0 : 1;
-		return playerNames[gameWinner]+" is the winner.";
+		
+		if (resultStr == ""){return null;}
+		
+		return resultStr; 
 	}
 
 	/**
